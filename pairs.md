9b8a28eb1d5ca07095477819938e440ef0634793
Jordan Castelloe <jordan.castelloe@gmail.com>
2020-03-21 08:47:04 -0400

    demo code for variables and data types

---
+++ pairs.md
@@ -0,0 +1,22 @@
+Group 1
+Derek M.
+Devin
+Mandy
+Austin
+
+Group 2
+Lindsey
+Sarah
+Barry
+Mike
+
+Group 3
+Swathi
+Stephen
+Pat
+
+Group 4
+Derek S.
+Dylan
+Jocelyn
+Ashon
---
+++ script.js
@@ -0,0 +1,111 @@
+// Declare a string variable to hold your favorite food
+
+var favoriteFood = "tacos";
+
+favoriteFood = "fish tacos";
+
+console.log(favoriteFood);
+
+var numberOfTacosIWouldLikeToEat = 17;
+
+var numberOfTacosTommyWouldLike = 20;
+
+var totalTacos = numberOfTacosIWouldLikeToEat + numberOfTacosTommyWouldLike;
+
+console.log(totalTacos);
+
+var isHungry = false;
+
+isHungry = true;
+
+
+// In your JavaScript file, instantiate and assign the following variables with information about yourself:
+
+// firstName should hold a string of your first name
+
+var firstName = "Jordan";
+
+// lastName should hold a string of your last name
+
+var lastName = "Castelloe";
+
+// age should hold a number of your age
+
+var age = 28;
+
+// likesTravel should hold a boolean representing whether or not you like to travel
+
+var likesToTravel = false;
+
+// Log each variable to the console.
+
+console.log(firstName, lastName, age, likesToTravel);
+
+
+
+
+// ----- LIGHTNING EXERCISE ----- //
+
+
+// ------ ARRAYS ----//
+// Array of ingredients to make a recipe
+
+var tacoIngredients = ["beef", "lettuce", "cheese", "beans"];
+
+console.log(tacoIngredients);
+
+console.log(tacoIngredients[0]);
+
+console.log(tacoIngredients[2]);
+
+tacoIngredients.push("chicken");
+
+console.log(tacoIngredients);
+
+// Access each item in the array individually-- start counting at zero!
+
+// Push a new item into the array
+
+
+
+
+
+// ---- LIGHTNING EXERCISE ---//
+
+
+
+// --- OBJECTS ---//
+// Object representing a person
+
+var jordanObject = {
+    firstName: "Jordan",
+    lastName: "Castelloe",
+    favoriteFood: "tacos",
+    hasADog: true,
+    students: ["Dylan", "Swathi", "Sarah"]
+}
+
+var tommyObject = {
+    firstName : "Tommy",
+    lastName: "Spurlock",
+    favoriteFood: "breakfast",
+    students: ["Derek", "Lindsey"]
+}
+
+var instructorList = [jordanObject, tommyObject]
+
+console.log(instructorList)
+
+
+
+// console.log(jordanObject.firstName)
+// console.log(jordanObject.hasADog)
+// console.log(jordanObject.students[0])
+
+
+// Add an array inside
+
+// Make an array of objects
+
+
+
